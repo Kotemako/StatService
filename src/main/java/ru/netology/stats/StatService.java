b6960a6d25c45c0e05b391322edc16cbc2a76ce8
Eugene Kotemako <kotemako@mail.ru>
2022-08-18 18:20:56 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatService.java
@@ -0,0 +1,43 @@
+package ru.netology.stats;
+
+public class StatService {
+
+    public long sum(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum = sum + sale;
+        }
+        return  sum;
+    }
+
+    public long average(long[] sales) {
+        return  sum(sales) / 12;
+
+    }
+
+    public int avgLess(long[] sales) {
+        long avg = average(sales);
+        int count = 0;
+        for (long sale : sales) {
+            if (sale < avg) {
+                count ++;
+            }
+        }
+        return count;
+
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+}
