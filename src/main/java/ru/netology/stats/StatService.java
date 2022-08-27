0530b5dc0ad642d86c71efedb77c0ea54388ba29
Eugene Kotemako <kotemako@mail.ru>
2022-08-27 20:54:02 +0300

    Add 2 test

--- src/main/java/ru/netology/stats/StatService.java
+++ src/main/java/ru/netology/stats/StatService.java
@@ -2,16 +2,28 @@ package ru.netology.stats;
 
 public class StatService {
 
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
     public long sum(long[] sales) {
         long sum = 0;
         for (long sale : sales) {
             sum = sum + sale;
         }
-        return  sum;
+        return sum;
     }
 
     public long average(long[] sales) {
-        return  sum(sales) / 12;
+        return sum(sales) / 12;
 
     }
 
@@ -20,24 +32,34 @@ public class StatService {
         int count = 0;
         for (long sale : sales) {
             if (sale < avg) {
-                count ++;
+                count++;
             }
         }
         return count;
 
     }
 
-    public int minSales(long[] sales) {
-        int minMonth = 0;
-        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
         for (long sale : sales) {
-            // sales[minMonth] - продажи в месяце minMonth
-            // sale - продажи в рассматриваемом месяце
-            if (sale <= sales[minMonth]) {
-                minMonth = month;
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
             }
-            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+            month = month + 1;
         }
-        return minMonth + 1;
+        return maxMonth + 1;
+    }
+
+    public int avgOver(long[] sales) {
+        long avg = average(sales);
+        int count = 0;
+        for (long sale : sales) {
+            if (sale > avg) {
+                count++;
+            }
+        }
+        return count;
+
     }
 }
--- src/test/java/ru/netology/stats/StatServiceTest.java
+++ src/test/java/ru/netology/stats/StatServiceTest.java
@@ -9,11 +9,12 @@ public class StatServiceTest {
     @Test
     public void test() {
         StatService service = new StatService();
-        long [] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         long actual = service.sum(sales);
         long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
 
         assertEquals(expected, actual);
     }
+
 }
