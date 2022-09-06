ed3afa4ce79ce97f24041368f52364d95a08b4a4
Eugene Kotemako <kotemako@mail.ru>
2022-09-06 23:50:42 +0300

    Add 6 test

--- src/test/java/ru/netology/stats/StatServiceTest.java
+++ src/test/java/ru/netology/stats/StatServiceTest.java
@@ -1,13 +1,15 @@
 package ru.netology.stats;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 public class StatServiceTest {
 
     @Test
-    public void test() {
+    public void sum() {
         StatService service = new StatService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
@@ -17,4 +19,58 @@ public class StatServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void average() {
+        StatService service = new StatService();
+        long[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedAverage = 15;
+        long actualAverage = service.average(array);
+
+        Assertions.assertEquals(expectedAverage, actualAverage);
+    }
+
+    @Test
+    public void maxSales() {
+        StatService service = new StatService();
+        long[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMaxDay = 8;
+        long actualMaxDay = service.maxSales(array);
+
+        Assertions.assertEquals(expectedMaxDay, actualMaxDay);
+    }
+
+    @Test
+    public void minSales() {
+        StatService service = new StatService();
+        long[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMinDay = 9;
+        long actualMinDay = service.minSales(array);
+
+        Assertions.assertEquals(expectedMinDay, actualMinDay);
+    }
+
+    @Test
+    public void avgLess() {
+        StatService service = new StatService();
+        long[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedLessAverage = 5;
+        long actualLessAverage = service.avgLess(array);
+
+        Assertions.assertEquals(expectedLessAverage, actualLessAverage);
+    }
+
+    @Test
+    public void avgOver() {
+        StatService service = new StatService();
+        long[] array = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMoreAverage = 5;
+        long actualMoreAverage = service.avgOver(array);
+
+        Assertions.assertEquals(expectedMoreAverage, actualMoreAverage);
+    }
 }
